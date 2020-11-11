cc8ff938a85c4ea2ebabde8da72258d25c682142
vancapelledavid <71630143+vancapelledavid@users.noreply.github.com>
2020-11-11 22:55:05 +0100

    Add files via upload

---
+++ opdracht20_js-comments/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten")
+//dit is een grote som
+// let sum = (1230941 * 1823794)
+// console.log(sum)
+// let sum2 = (637263 / 54)
+// console.log(sum2)
+// //probeersel
+// console.log(45+9)
